685af2042cc87238e8d364271a10adec4a9afcc5
Nouf <nalknedri@stu.kau.edu.sa>
2024-04-18 15:47:09 +0300

    Initial Commit

---
+++ ApplicationToDoList/ApplicationToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationToDoListApp.swift
+//  ApplicationToDoList
+//
+//  Created by Nouf Faisal  on 09/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
